40eb380ef7fade9a59a01a7d32879d1422f54127
Moha <moha.as172@gmail.com>
2022-11-06 12:26:45 +0100

    probando exposed

--- tennislab/src/main/kotlin/Main.kt
+++ tennislab/src/main/kotlin/Main.kt
@@ -1,7 +1,36 @@
-fun main(args: Array<String>) {
-    println("Hello World!")
+import models.Usuario
+import models.Usuarios
+import org.jetbrains.exposed.sql.Database
+import org.jetbrains.exposed.sql.SchemaUtils
+import org.jetbrains.exposed.sql.StdOutSqlLogger
+import org.jetbrains.exposed.sql.addLogger
+import org.jetbrains.exposed.sql.transactions.transaction
+import java.sql.DriverManager
 
-    // Try adding program arguments via Run/Debug configuration.
-    // Learn more about running applications: https://www.jetbrains.com/help/idea/running-applications.html.
-    println("Program arguments: ${args.joinToString()}")
+fun main(args: Array<String>) {
+    Database.connect("jdbc:h2:mem:test", driver = "org.h2.Driver")
+
+    transaction {
+        addLogger(StdOutSqlLogger)
+        SchemaUtils.create(Usuarios)
+
+        create()
+        val usuario = read()
+        println(usuario)
+    }
+}
+
+fun create(): Usuario {
+    val usuario = Usuario.new {
+        nombre = "Nombre"
+        apellido = "Apellido"
+        email = "Email@email.com"
+        password = "1234"
+    }
+    return usuario
+}
+
+fun read(): Usuario {
+    val usuario = Usuario.find { Usuarios.nombre eq "Nombre" }
+    return usuario.first()
 }
