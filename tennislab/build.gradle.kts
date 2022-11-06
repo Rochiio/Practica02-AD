5e0d7eebe5b99def2c426bd46858c5d7f9959a33
Rocio <rpalaofernandez@gmail.com>
2022-11-06 10:16:29 +0100

    db

--- tennislab/build.gradle.kts
+++ tennislab/build.gradle.kts
@@ -14,6 +14,20 @@ repositories {
 
 dependencies {
     testImplementation(kotlin("test"))
+    // Exposed
+    implementation("org.jetbrains.exposed:exposed-core:0.39.2")
+    implementation("org.jetbrains.exposed:exposed-dao:0.39.2")
+    implementation("org.jetbrains.exposed:exposed-jdbc:0.39.2")
+    // Base de Datos H2 Driver JDBC
+    implementation("com.h2database:h2:2.1.214")
+    // Para hacer el logging
+    implementation("io.github.microutils:kotlin-logging-jvm:3.0.0")
+    implementation("ch.qos.logback:logback-classic:1.4.1")
+    // Para manejar las fechas
+    implementation("org.jetbrains.exposed:exposed-java-time:0.39.2")
+    // Opcionales
+    // Para manejar un pool de conexions mega rápido con HikariCP (no es obligatorio)
+    implementation("com.zaxxer:HikariCP:5.0.1")
 }
 
 tasks.test {
