94620a874435dd6d774449329b00beffe005c639
Rocio <rpalaofernandez@gmail.com>
2022-11-08 16:51:08 +0100

    cambios repo usuario

--- tennislab/src/main/kotlin/repositories/ICRUD.kt
+++ tennislab/src/main/kotlin/repositories/ICRUD.kt
@@ -5,8 +5,8 @@ import java.util.UUID
 interface ICRUD<T,ID> {
     fun findById(id: ID):T?
     fun findByUUID(uuid: UUID):T?
-    fun add(item: T):T
-    fun update(item: T):T?
-    fun delete(item: T):T?
-    fun findAll():List<T>?
+    fun add(item: T):T?
+    fun update(item: T, updateItem: T):T
+    fun delete(item: T):Boolean
+    fun findAll():List<T>
 }
--- tennislab/src/main/kotlin/repositories/usuarios/UsuarioRepositoryImpl.kt
+++ tennislab/src/main/kotlin/repositories/usuarios/UsuarioRepositoryImpl.kt
@@ -2,42 +2,64 @@ package repositories.usuarios
 
 import models.Usuario
 import models.Usuarios
-import models.enums.tipoUsuario
-import org.jetbrains.exposed.sql.insert
-import org.jetbrains.exposed.sql.insertAndGetId
+import mu.KotlinLogging
+import org.jetbrains.exposed.sql.transactions.transaction
 import java.util.*
 
+
 class UsuarioRepositoryImpl: UsuarioRepository {
-    override fun findById(id: Int): Usuario? {
-        return Usuario.findById(id)
+    private var logger = KotlinLogging.logger {}
+    override fun findById(id: Int): Usuario? = transaction {
+        logger.debug { "Buscando usuario por id: $id" }
+        Usuario.findById(id)
     }
 
-    override fun findByUUID(uuid: UUID): Usuario? {
-        return Usuario.find { Usuarios.uuid eq uuid }.firstOrNull()
+    override fun findByUUID(uuid: UUID): Usuario? = transaction{
+       logger.debug{"Buscando por uuid: $uuid"}
+        Usuario.find { Usuarios.uuid eq uuid }.firstOrNull()
     }
 
-    override fun add(item: Usuario): Usuario {
-        val user = Usuario.new {
-            nombre = item.nombre
-            apellido = item.apellido
-            email = item.email
-            password = item.password
-            tipo = item.tipo
-            disponible = item.disponible
+    override fun add(item: Usuario): Usuario = transaction{
+        logger.debug { "Añadiendo usuario" }
+        val existe = Usuario.find { Usuarios.uuid eq item.uuid }.firstOrNull()
+        existe?.let {
+            update(item, existe)
+        } ?: run {
+            Usuario.new {
+                nombre=item.nombre
+                apellido = item.apellido
+                email = item.email
+                password = item.password
+                tipo = item.tipo
+                disponible = item.disponible
+            }
         }
-
-        return user
     }
 
-    override fun update(item: Usuario): Usuario? {
-        TODO("Not yet implemented")
+    override fun update(item: Usuario, updateItem: Usuario): Usuario = transaction{
+        logger.debug { "Actualizando Usuario" }
+        item.apply{
+            nombre=updateItem.nombre
+            apellido = updateItem.apellido
+            email = updateItem.email
+            password = updateItem.password
+            tipo = updateItem.tipo
+            disponible = updateItem.disponible
+        }
     }
 
-    override fun delete(item: Usuario): Usuario? {
-        TODO("Not yet implemented")
+    override fun delete(item: Usuario): Boolean =transaction{
+        logger.debug { "Eliminando Usuario" }
+      val existe = Usuario.find { Usuarios.uuid eq item.uuid }.firstOrNull()?: return@transaction false
+      existe.delete()
+      return@transaction true
     }
 
-    override fun findAll(): List<Usuario>? {
-        TODO("Not yet implemented")
+    override fun findAll(): List<Usuario> = transaction {
+        logger.debug { "Buscando todos los usuarios" }
+        return@transaction Usuario.all().map { it }.toList()
     }
+
+
+
 }
