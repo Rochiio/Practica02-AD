5831f9e1b62d0a90ea2ddacf5a9731cdbfaf7907
Rocio <rpalaofernandez@gmail.com>
2022-11-09 18:01:45 +0100

    test usuarios

--- tennislab/src/main/kotlin/repositories/usuarios/UsuarioRepositoryImpl.kt
+++ tennislab/src/main/kotlin/repositories/usuarios/UsuarioRepositoryImpl.kt
@@ -9,16 +9,34 @@ import java.util.*
 
 class UsuarioRepositoryImpl: UsuarioRepository {
     private var logger = KotlinLogging.logger {}
+
+    /**
+     * Buscar a un usuario por su id.
+     * @param id id del usuario.
+     * @return usuario si existe.
+     */
     override fun findById(id: Int): Usuario? = transaction {
         logger.debug { "Buscando usuario por id: $id" }
         Usuario.findById(id)
     }
 
+
+    /**
+     * Buscar un usuario por su uuid.
+     * @param uuid del usuario.
+     * @return usuario si existe.
+     */
     override fun findByUUID(uuid: UUID): Usuario? = transaction{
        logger.debug{"Buscando por uuid: $uuid"}
         Usuario.find { Usuarios.uuid eq uuid }.firstOrNull()
     }
 
+
+    /**
+     * Añadir un usuario.
+     * @param item usuario a añadir.
+     * @return usuario añadido u actualizado si ya existía.
+     */
     override fun add(item: Usuario): Usuario = transaction{
         logger.debug { "Añadiendo usuario" }
         val existe = Usuario.find { Usuarios.uuid eq item.uuid }.firstOrNull()
@@ -36,6 +54,13 @@ class UsuarioRepositoryImpl: UsuarioRepository {
         }
     }
 
+
+    /**
+     * Actualizar usuario.
+     * @param item usuario a actualizar.
+     * @param updateItem usuario actualizado.
+     * @return usuario ya actualizado.
+     */
     override fun update(item: Usuario, updateItem: Usuario): Usuario = transaction{
         logger.debug { "Actualizando Usuario" }
         item.apply{
@@ -48,13 +73,24 @@ class UsuarioRepositoryImpl: UsuarioRepository {
         }
     }
 
+
+    /**
+     * Eliminar un usuario
+     * @param item usuario a eliminar.
+     * @return boolean dependiendo de si ha realizado correctamente
+     */
     override fun delete(item: Usuario): Boolean =transaction{
         logger.debug { "Eliminando Usuario" }
       val existe = Usuario.find { Usuarios.uuid eq item.uuid }.firstOrNull()?: return@transaction false
-      existe.delete()
+      existe.disponible = false
       return@transaction true
     }
 
+
+    /**
+     * Encontrar todos los usuarios que hay.
+     * @return lista de usuarios.
+     */
     override fun findAll(): List<Usuario> = transaction {
         logger.debug { "Buscando todos los usuarios" }
         return@transaction Usuario.all().map { it }.toList()
