568afea40d3dcee0787c702545d49da799d90d40
Rocio <rpalaofernandez@gmail.com>
2022-11-18 18:19:16 +0100

    serializadores creados, pedidos serializable listo

--- tennislab/src/main/kotlin/models/pedidos/Pedido.kt
+++ tennislab/src/main/kotlin/models/pedidos/Pedido.kt
@@ -2,18 +2,26 @@ package models.pedidos
 
 import entities.enums.Estado
 import kotlinx.serialization.Serializable
+import kotlinx.serialization.Serializer
+import kotlinx.serialization.serializer
+import utils.serializer.LocalDateSerializer
+import utils.serializer.UUIDSerializer
 import java.time.LocalDate
 import java.util.*
 
-//TODO hacer serializadores
 @Serializable
 data class Pedido(
+    @Serializable(with = UUIDSerializer::class)
     var uuid: UUID?,
     val estado: Estado,
+    @Serializable(with = LocalDateSerializer::class)
     val fechaEntrada: LocalDate,
+    @Serializable(with = LocalDateSerializer::class)
     val fechaSalida: LocalDate,
+    @Serializable(with = LocalDateSerializer::class)
     val fechaFinal: LocalDate,
-    val precioTotal: Float
+    val precioTotal: Float,
+    @Serializable(with = LocalDateSerializer::class)
     val topeEntrega: LocalDate,
     ) {
 }
